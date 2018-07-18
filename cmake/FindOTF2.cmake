5789980dbf752ee85338600fc5b3cf246cb20794
Alexander Grund <github@grundis.de>
2018-07-18 15:40:19 +0000

    Generalize FindOTF2
    This adds an extra module that abstracts the usage of a config-exe. It is build like the CMake findPkgConfig module and works for more than just OTF2. It can e.g. be used for ScoreP too.

--- cmake/FindOTF2.cmake
+++ cmake/FindOTF2.cmake
@@ -1,89 +1,51 @@
-option(OTF2_USE_STATIC_LIBS "Link OTF2 statically." ON)
+option(OTF2_USE_STATIC_LIBS "Link OTF2 statically." OFF)
 if(OTF2_USE_STATIC_LIBS)
   set(_OTF2_ORIG_CMAKE_FIND_LIBRARY_SUFFIXES ${CMAKE_FIND_LIBRARY_SUFFIXES})
   set(CMAKE_FIND_LIBRARY_SUFFIXES .a)
 endif()
 
-IF(OTF2_CONFIG_PATH)
-    FIND_PROGRAM(OTF2_CONFIG NAMES otf2-config
-        PATHS
-        /opt/otf2/bin
-        HINTS
-        ${OTF2_CONFIG_PATH}
-    )
-ELSE(OTF2_CONFIG_PATH)
-    FIND_PROGRAM(OTF2_CONFIG NAMES otf2-config
-        PATHS
-        /opt/otf2/bin
-    )
-ENDIF(OTF2_CONFIG_PATH)
-
-IF(NOT OTF2_CONFIG OR NOT EXISTS ${OTF2_CONFIG})
-    MESSAGE(STATUS "no otf2-config found")
-    SET(OTF2_VERSION ${OTF2_REQUIRED_VERSION})
-ELSE()
-    message(STATUS "OTF2 library found. (using ${OTF2_CONFIG})")
-
-    execute_process(COMMAND ${OTF2_CONFIG} "--interface-version" OUTPUT_VARIABLE OTF2_VERSION)
-    STRING(REPLACE ":" "." OTF2_VERSION ${OTF2_VERSION})
-    STRING(STRIP ${OTF2_VERSION} OTF2_VERSION)
-
-    execute_process(COMMAND ${OTF2_CONFIG} "--cppflags" OUTPUT_VARIABLE OTF2_INCLUDE_DIRS)
-    STRING(REPLACE "\n" "" OTF2_INCLUDE_DIRS ${OTF2_INCLUDE_DIRS})
-    STRING(REPLACE "-I" ";" OTF2_INCLUDE_DIRS ${OTF2_INCLUDE_DIRS})
-
-    execute_process(COMMAND ${OTF2_CONFIG} "--ldflags" OUTPUT_VARIABLE _LINK_LD_ARGS)
-    STRING( REPLACE " " ";" _LINK_LD_ARGS ${_LINK_LD_ARGS} )
-    FOREACH( _ARG ${_LINK_LD_ARGS} )
-        IF(${_ARG} MATCHES "^-L")
-            STRING(REGEX REPLACE "^-L" "" _ARG ${_ARG})
-            STRING(STRIP "${_ARG}" _ARG)
-            SET(OTF2_LINK_DIRS ${OTF2_LINK_DIRS} ${_ARG})
-        ENDIF(${_ARG} MATCHES "^-L")
-    ENDFOREACH(_ARG)
-
-    execute_process(COMMAND ${OTF2_CONFIG} "--libs" OUTPUT_VARIABLE _LINK_LD_ARGS)
-    STRING( REPLACE " " ";" _LINK_LD_ARGS ${_LINK_LD_ARGS} )
-    FOREACH( _ARG ${_LINK_LD_ARGS} )
-        IF(${_ARG} MATCHES "^-l")
-            STRING(REGEX REPLACE "^-l" "" _ARG "${_ARG}")
-            STRING(STRIP "${_ARG}" _ARG)
-            # NO_DEFAULT_PATH - We have to "filter" -lm, as g++ links it anyways. And then stuff explodes
-            FIND_LIBRARY(_OTF2_LIB_FROM_ARG NAMES ${_ARG}
-                HINTS ${OTF2_LINK_DIRS} NO_DEFAULT_PATH
-            )
-            IF(_OTF2_LIB_FROM_ARG)
-                SET(OTF2_LIBRARIES ${OTF2_LIBRARIES} ${_OTF2_LIB_FROM_ARG})
-            ENDIF(_OTF2_LIB_FROM_ARG)
-            UNSET(_OTF2_LIB_FROM_ARG CACHE)
-        ENDIF(${_ARG} MATCHES "^-l")
-    ENDFOREACH(_ARG)
-
-    find_program(OTF2_PRINT "otf2-print" PATHS "${OTF2_LINK_DIRS}/.." PATH_SUFFIXES "bin")
-ENDIF()
-
-include (FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(OTF2
-    FOUND_VAR OTF2_FOUND
-    REQUIRED_VARS OTF2_CONFIG OTF2_LIBRARIES OTF2_INCLUDE_DIRS OTF2_PRINT
-    VERSION_VAR OTF2_VERSION
+include(CfgExeSearchModule)
+cfg_exe_search_module(OTF2 otf2-config
+  HINTS ENV OTF2_ROOT "${OTF2_CONFIG_PATH}"
+  PATHS /opt/otf2
+  VERSION_FLAG --interface-version
+  IMPORTED_TARGET
 )
+string(REPLACE ":" "." OTF2_VERSION "${OTF2_VERSION}")
+if(OTF2_CONFIG)
+  get_filename_component(config_path "${OTF2_CONFIG}" DIRECTORY)
+  find_program(OTF2_PRINT "otf2-print" HINTS "${config_path}" "${OTF2_CONFIG_PATH}")
 
-if(OTF2_FOUND)
-    message(STATUS "OTF2 interface version: ${OTF2_VERSION}")
-
-    add_library(otf2::otf2 UNKNOWN IMPORTED GLOBAL)
-    set_target_properties(otf2::otf2 PROPERTIES
-        IMPORTED_LINK_INTERFACE_LANGUAGES "C"
-        IMPORTED_LOCATION "${OTF2_LIBRARIES}"
-        INTERFACE_INCLUDE_DIRECTORIES "${OTF2_INCLUDE_DIRS}"
+  if(python_bindings IN_LIST OTF2_FIND_COMPONENTS)
+    execute_process (COMMAND "${OTF2_CONFIG}" --pythonpath
+        RESULT_VARIABLE otf2_result
+        OUTPUT_VARIABLE OTF2_python_bindings
+        OUTPUT_STRIP_TRAILING_WHITESPACE
     )
-
-else()
-    unset(OTF2_PRINT)
-    unset(OTF2_LINK_DIRS)
-    unset(OTF2_LIBRARIES)
+    set(OTF2_python_bindings_FOUND 0)
+    if(otf2_result AND EXISTS "${OTF2_python_bindings}")
+      set(OTF2_python_bindings_FOUND 1)
+    else()
+      get_filename_component(otf2_bin_path "${OTF2_CONFIG_EXE}" DIRECTORY)
+      get_filename_component(otf2_root_path "${otf2_bin_path}" DIRECTORY)
+      find_path (OTF2_python_bindings
+        otf2/__init__.py
+        HINTS "${otf2_bin_path}" "${otf2_root_path}" ENV OTF2_ROOT
+        PATH_SUFFIXES lib/python2.7/site-packages python2.7/site-packages
+      )
+      if (OTF2_python_bindings)
+        set(OTF2_python_bindings_FOUND 1)
+      endif()
+    endif()
+  endif()
 endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(OTF2
+  REQUIRED_VARS OTF2_CONFIG OTF2_PRINT OTF2_INCLUDE_DIRS ${OTF2_LIB_VARS}
+  VERSION_VAR OTF2_VERSION
+  HANDLE_COMPONENTS
+)
 
 if(OTF2_USE_STATIC_LIBS)
   set(CMAKE_FIND_LIBRARY_SUFFIXES ${_OTF2_ORIG_CMAKE_FIND_LIBRARY_SUFFIXES})
