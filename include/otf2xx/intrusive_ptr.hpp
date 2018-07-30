0304a5b6c730ef9e515a4bfa1353585d2fcb92b8
Alexander Grund <github@grundis.de>
2018-07-30 14:53:33 +0200

    Add self-handle assignment check

--- include/otf2xx/intrusive_ptr.hpp
+++ include/otf2xx/intrusive_ptr.hpp
@@ -35,111 +35,115 @@
 #ifndef INCLUDE_OTF2XX_INTRUSIVE_PTR_HPP
 #define INCLUDE_OTF2XX_INTRUSIVE_PTR_HPP
 
+#include <utility>
+
 namespace otf2
 {
 
-    /**
-     * \brief shared_ptr using the contained class for ref counting
-     *
-     * This is similar to shared_ptr but requires the contained type to define
-     * the functions `retain()` to increase the ref counter and
-     * `release()` to decrease it and return the new value.
-     *
-     * This is very similar to boost::intrusive_ptr.
-     *
-     * \tparam T contained type
-     */
-    template <class T>
-    class intrusive_ptr
+/**
+ * \brief shared_ptr using the contained class for ref counting
+ *
+ * This is similar to shared_ptr but requires the contained type to define
+ * the functions `retain()` to increase the ref counter and
+ * `release()` to decrease it and return the new value.
+ *
+ * This is very similar to boost::intrusive_ptr.
+ *
+ * \tparam T contained type
+ */
+template <class T>
+class intrusive_ptr
+{
+public:
+    intrusive_ptr() : data_(nullptr)
     {
-    public:
-        intrusive_ptr() : data_(nullptr)
-        {
-        }
+    }
 
-        intrusive_ptr(T* data) : data_(data)
-        {
-            if (data_)
-            {
-                data_->retain();
-            }
-        }
+    explicit intrusive_ptr(T* data) : data_(data)
+    {
+        if (data_)
+            data_->retain();
+    }
 
-        intrusive_ptr(const intrusive_ptr& other) : intrusive_ptr(other.get())
-        {
-        }
+    intrusive_ptr(const intrusive_ptr& other) : intrusive_ptr(other.get())
+    {
+    }
 
-        intrusive_ptr(intrusive_ptr&& other) : intrusive_ptr()
-        {
-            this->swap(other);
-        }
+    intrusive_ptr(intrusive_ptr&& other) : intrusive_ptr()
+    {
+        this->swap(other);
+    }
 
-        intrusive_ptr& operator=(intrusive_ptr other)
-        {
-            this->swap(other);
+    ~intrusive_ptr()
+    {
+        if (data_ && data_->release() == 0u)
+            delete data_;
+    }
+
+    intrusive_ptr& operator=(const intrusive_ptr& other)
+    {
+        if (data_ == other.data_)
             return *this;
-        }
-
-        ~intrusive_ptr()
-        {
-            if (data_ && data_->release() == 0u)
-            {
-                delete data_;
-            }
-        }
-
-        void swap(intrusive_ptr& other)
-        {
-            T* tmp = data_;
-            data_ = other.data_;
-            other.data_ = tmp;
-        }
-
-        /**
-         * \brief Returns whether the contained pointer is not nullptr
-         *
-         * \returns true or false
-         */
-        explicit operator bool() const
-        {
-            return data_ != nullptr;
-        }
-
-        T& operator*() const
-        {
-            return *data_;
-        }
-
-        T* operator->() const
-        {
-            return data_;
-        }
-
-        T* get() const
-        {
-            return data_;
-        }
-
-    private:
-        T* data_;
-    };
-
-    template <class T>
-    inline bool operator==(const intrusive_ptr<T>& a, const intrusive_ptr<T>& b)
-    {
-        return a.get() == b.get();
+        intrusive_ptr(other).swap(*this);
+        return *this;
     }
 
-    template <class T>
-    inline bool operator!=(const intrusive_ptr<T>& a, const intrusive_ptr<T>& b)
+    intrusive_ptr& operator=(intrusive_ptr&& other)
     {
-        return !(a == b);
+        intrusive_ptr(std::move(other)).swap(*this);
+        return *this;
     }
-    template <class T>
-    inline void swap(intrusive_ptr<T>& a, intrusive_ptr<T>& b)
+
+    void swap(intrusive_ptr& other)
     {
-        a.swap(b);
+        std::swap(data_, other.data_);
     }
+
+    /**
+     * \brief Returns whether the contained pointer is not nullptr
+     *
+     * \returns true or false
+     */
+    explicit operator bool() const
+    {
+        return data_ != nullptr;
+    }
+
+    T& operator*() const
+    {
+        return *data_;
+    }
+
+    T* operator->() const
+    {
+        return data_;
+    }
+
+    T* get() const
+    {
+        return data_;
+    }
+
+private:
+    T* data_;
+};
+
+template <class T>
+inline bool operator==(const intrusive_ptr<T>& a, const intrusive_ptr<T>& b)
+{
+    return a.get() == b.get();
+}
+
+template <class T>
+inline bool operator!=(const intrusive_ptr<T>& a, const intrusive_ptr<T>& b)
+{
+    return !(a == b);
+}
+template <class T>
+inline void swap(intrusive_ptr<T>& a, intrusive_ptr<T>& b)
+{
+    a.swap(b);
+}
 } // namespace otf2
 
 #endif // INCLUDE_OTF2XX_INTRUSIVE_PTR_HPP
