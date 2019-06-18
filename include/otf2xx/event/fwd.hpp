024dc9a30a7a4480d26d620403e9485d59cd86dc
Mario Bielert <mario.bielert@tu-dresden.de>
2019-06-18 10:43:45 +0200

    reorder rma event forwards

--- include/otf2xx/event/fwd.hpp
+++ include/otf2xx/event/fwd.hpp
@@ -72,24 +72,24 @@ namespace event
     class calling_context_leave;
     class calling_context_sample;
 
-    class rma_win_create;
-    class rma_win_destroy;
+    class rma_acquire_lock;
+    class rma_atomic;
     class rma_collective_begin;
     class rma_collective_end;
-    class rma_group_sync;
-    class rma_request_lock;
-    class rma_acquire_lock;
-    class rma_try_lock;
-    class rma_release_lock;
-    class rma_sync;
-    class rma_wait_change;
-    class rma_put;
     class rma_get;
-    class rma_atomic;
+    class rma_group_sync;
     class rma_op_complete_blocking;
     class rma_op_complete_non_blocking;
     class rma_op_test;
     class rma_op_complete_remote;
+    class rma_put;
+    class rma_release_lock;
+    class rma_request_lock;
+    class rma_sync;
+    class rma_try_lock;
+    class rma_wait_change;
+    class rma_win_create;
+    class rma_win_destroy;
 
     class thread_fork;
     class thread_join;
